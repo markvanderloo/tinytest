8f8297df8a1be2dabaa7359c4477739602f1f806
Mark van der Loo <mark.vanderloo@gmail.com>
2019-03-20 23:04:15 +0100

    added local test script

---
+++ test.r
@@ -0,0 +1,19 @@
+#!/usr/bin/Rscript
+
+pkg <- "tinytest"
+
+system("R CMD build --no-build-vignettes --no-manual ./pkg")
+dr <- tempfile()
+dir.create(dr)
+
+pkgfile <- dir("./",pattern=paste0(pkg, ".*\\.tar\\.gz"))
+
+
+install.packages(pkgfile,lib=dr, repos=NULL)
+
+library(pkg, lib.loc=dr, character.only=TRUE)
+
+run_test_dir(system.file("utst", package=pkg))
+
+
+
