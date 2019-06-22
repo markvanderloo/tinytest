36d55b95cd50cb0adcde0edb7f4001838d815da2
Mark van der Loo <mark.vanderloo@gmail.com>
2019-06-22 21:18:10 +0200

    do not print OK if no tests are run in a file

--- pkg/R/tinytest.R
+++ pkg/R/tinytest.R
@@ -648,7 +648,9 @@ run_test_file <- function( file
     
     # print the test counter. 
     catf("\r%s %4d tests ", prfile, o$ntest())
-    if ( o$nfail() == 0 ) catf(if(color) "\033[0;32mOK\033[0m" else "OK")
+    # print status after counter
+    if ( o$ntest() == 0 ) {} # print nothing if nothing was tested
+    else if ( o$nfail() == 0) catf(if(color) "\033[0;32mOK\033[0m" else "OK")
     else catf(if (color) "\033[0;31m%d errors\033[0m" else "%d errors", o$nfail())
   }
   catf("\n")
