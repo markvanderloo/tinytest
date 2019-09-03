637cee89a129509702be5f0efc5c6865f3812238
Mark van der Loo <mark.vanderloo@gmail.com>
2019-09-03 16:47:35 +0200

    removed malware detector

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -5,8 +5,6 @@ stopf <- function(fmt,...) stop(sprintf(fmt,...), call.=FALSE)
 warnf <- function(fmt,...) warning(sprintf(fmt,...), call.=FALSE)
 msgf  <- function(fmt, ...) message(sprintf(fmt,...))
 
-# detect malware
-on_windows <- function() Sys.info()[["sysname"]] == "Windows"
 
 
 
