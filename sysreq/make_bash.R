a9d315818c03a77c3781e63a9f19999b1ca0502b
Mark van der Loo <mark.vanderloo@gmail.com>
2020-04-24 22:10:26 +0200

    technicalities

---
+++ sysreq/make_bash.R
@@ -0,0 +1,52 @@
+
+
+#  Clone RStudio's list of system requirements and 
+#  create a bash file that installs them for ubuntu
+make_bash <- function(){
+  pwd <- getwd()
+  on.exit(setwd(pwd))
+  
+  # go to temp dir to get data from RStudio's repo
+  wd <- tempfile()
+  dir.create(wd)
+  setwd(wd)
+  
+  system("git clone https://github.com/rstudio/r-system-requirements")
+  
+  # get the json files
+  jsonfiles <- dir("r-system-requirements/rules", pattern="\\.json$", full.names = TRUE)
+  
+  deps <- vector(mode="character",length=length(jsonfiles))
+  
+  # harvest package names for ubuntu
+  for ( i in seq_along(jsonfiles) ){
+    json <- jsonlite::fromJSON(jsonfiles[i])
+    ubuntu <- sapply(json$dependencies$constraints, function(x) any(grepl("ubuntu",x$distribution)))
+    if (any(ubuntu)){
+      deps[i] <- paste(unique(Reduce(c, json$dependencies$packages[ubuntu])), collapse=" ")
+    }
+  }
+  
+  depstring <- paste(sprintf("sudo apt install --assume-yes %s ",deps), collapse="\n")
+  
+  template <- 
+    "
+#!/bin/bash
+
+%s
+
+"
+  # create runnable bash script
+  bash <- sprintf(template, depstring)
+  
+  bash
+}
+
+
+bash <- make_bash()
+write(bash, file="install_them_all.sh")
+
+
+
+
+
