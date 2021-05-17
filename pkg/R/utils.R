90fc31c11f3d37652b77d5a4d358bc88d727dd1d
Matthijs S. Berends <31037261+msberends@users.noreply.github.com>
2021-05-18 00:12:30 +0200

    Update utils.R

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -5,6 +5,13 @@ stopf <- function(fmt,...) stop(sprintf(fmt,...), call.=FALSE)
 warnf <- function(fmt,...) warning(sprintf(fmt,...), call.=FALSE)
 msgf  <- function(fmt, ...) message(sprintf(fmt,...))
 
-
-
-
+# support R < 3.2, that lacks trimws()
+trimws <- function(x, which = c("both", "left", "right")) {
+  which <- match.arg(which)
+  mysub <- function(re, x) sub(re, "", x, perl = TRUE)
+  if (which == "left")
+    return(mysub("^[ \t\r\n]+", x))
+  if (which == "right")
+    return(mysub("[ \t\r\n]+$", x))
+  mysub
+}
