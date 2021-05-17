f7fea54b281e56dac9d633eafb157234be7cbb49
Matthijs S. Berends <31037261+msberends@users.noreply.github.com>
2021-05-18 00:18:20 +0200

    Update utils.R

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -5,13 +5,11 @@ stopf <- function(fmt,...) stop(sprintf(fmt,...), call.=FALSE)
 warnf <- function(fmt,...) warning(sprintf(fmt,...), call.=FALSE)
 msgf  <- function(fmt, ...) message(sprintf(fmt,...))
 
-# support R < 3.2, that lacks trimws()
-trimws <- function(x, which = c("both", "left", "right")) {
-  which <- match.arg(which)
-  mysub <- function(re, x) sub(re, "", x, perl = TRUE)
-  if (which == "left")
-    return(mysub("^[ \t\r\n]+", x))
-  if (which == "right")
-    return(mysub("[ \t\r\n]+$", x))
-  mysub
+# support R versions < 3.2, that lack trimws()
+trimws <- function (x, which = c("both", "left", "right"), whitespace = "[ \t\r\n]") {
+    which <- match.arg(which)
+    mysub <- function(re, x) sub(re, "", x, perl = TRUE)
+    switch(which, left = mysub(paste0("^", whitespace, "+"), 
+        x), right = mysub(paste0(whitespace, "+$"), x), both = mysub(paste0(whitespace, 
+        "+$"), mysub(paste0("^", whitespace, "+"), x)))
 }
