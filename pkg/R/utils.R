be1f7863cb3dfab8b3ae33986a01dd803b3c60a9
Matthijs S. Berends <31037261+msberends@users.noreply.github.com>
2021-05-20 11:22:57 +0200

    Update utils.R

--- pkg/R/utils.R
+++ pkg/R/utils.R
@@ -5,7 +5,7 @@ stopf <- function(fmt,...) stop(sprintf(fmt,...), call.=FALSE)
 warnf <- function(fmt,...) warning(sprintf(fmt,...), call.=FALSE)
 msgf  <- function(fmt, ...) message(sprintf(fmt,...))
 
-# support R versions < 3.2, that lack trimws()
+# support R versions < 3.2, that lack trimws() and dir.exists()
 trimws <- function (x, which = c("both", "left", "right"), whitespace = "[ \t\r\n]") {
   which <- match.arg(which)
   mysub <- function(re, x) sub(re, "", x, perl = TRUE)
@@ -13,4 +13,8 @@ trimws <- function (x, which = c("both", "left", "right"), whitespace = "[ \t\r\
          left = mysub(paste0("^", whitespace, "+"), x),
          right = mysub(paste0(whitespace, "+$"), x),
          both = mysub(paste0(whitespace, "+$"), mysub(paste0("^", whitespace, "+"), x)))
+}
+dir.exists <- function (paths) {
+  x = base::file.info(paths)$isdir
+  !is.na(x) & x
 }
